92caba843b45458e6739b2edf1e5a8bff18dde3d
angelinalex <60354819+angelinalex@users.noreply.github.com>
2020-02-04 14:03:45 -0500

    Picture

--- Picture.java
+++ Picture.java
@@ -12,14 +12,6 @@
 public class Picture
 {
     private Square body;
-    private Square arm1;
-    private Square arm2;
-    private Square leg1;
-    private Square leg2;
-    private Triangle hat; //test to see if works
-    private Circle mouth;
-    private Circle eye1;
-    private Circle eye2;
     private boolean drawn;
 
     /**
@@ -28,14 +20,7 @@ public class Picture
     public Picture()
     {
         body = new Square();
-        arm1 = new Square();
-        arm2 = new Square();
-        leg1 = new Square();
-        leg2 = new Square();
-        hat = new Triangle();  
-        mouth = new Circle();
-        eye1 = new Circle();
-        eye2 = new Circle();
+       
         drawn = false;
     }
 
@@ -50,62 +35,7 @@ public class Picture
             body.changeSize(120);
             body.changeColor("blue");
             body.makeVisible();
-            
-            arm1.moveHorizontal(-140);
-            arm1.moveVertical(20);
-            arm1.changeSize(120);
-            arm1.changeColor("blue");
-            arm1.makeVisible();
-            
-            arm2.moveHorizontal(-140);
-            arm2.moveVertical(20);
-            arm2.changeSize(120);
-            arm2.changeColor("blue");
-            arm2.makeVisible();
-            
-            leg1.moveHorizontal(-140);
-            leg1.moveVertical(20);
-            leg1.changeSize(120);
-            leg1.changeColor("blue");
-            leg1.makeVisible();
-            
-            leg2.moveHorizontal(-140);
-            leg2.moveVertical(20);
-            leg2.changeSize(120);
-            leg2.changeColor("blue");
-            leg2.makeVisible();
-            
-            hat.moveHorizontal(-140);
-            hat.moveVertical(20);
-            hat.changeSize(120);
-            hat.changeColor("blue");
-            hat.makeVisible();
-            
-            mouth.changeColor("black");
-            mouth.moveHorizontal(-120);
-            mouth.moveVertical(40);
-            mouth.changeSize(40);
-            mouth.makeVisible();
-             
-            eye1.changeColor("black");
-            eye1.changeSize(60, 180);
-            eye1.moveHorizontal(20);
-            eye1.moveVertical(-60);
-            eye1.makeVisible();
-            
-            eye2.changeColor("black");
-            eye2.changeSize(60, 180);
-            eye2.moveHorizontal(20);
-            eye2.moveVertical(-60);
-            eye1.makeVisible();
-    
-            
-            sun.changeColor("yellow");
-            sun.moveHorizontal(100);
-            sun.moveVertical(-40);
-            sun.changeSize(80);
-            sun.makeVisible();
-            drawn = true;
+           
         }
     }
 
@@ -114,10 +44,8 @@ public class Picture
      */
     public void setBlackAndWhite()
     {
-        wall.changeColor("black");
-        window.changeColor("white");
-        roof.changeColor("black");
-        sun.changeColor("black");
+        body.changeColor("black");
+       
     }
 
     /**
@@ -125,9 +53,7 @@ public class Picture
      */
     public void setColor()
     {
-        wall.changeColor("red");
-        window.changeColor("black");
-        roof.changeColor("green");
-        sun.changeColor("yellow");
+        body.changeColor("red");
+       
     }
 }
